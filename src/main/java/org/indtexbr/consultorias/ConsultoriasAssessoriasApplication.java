91544ca4f40bf1d8817863cebc6c4beb4e20bd42
Diogo Barbosa <diogoalmeidabarbosa@gmail.com>
2021-11-25 01:16:10 -0300

    Corrigindo letra faltante em algumas variaveis.

--- src/main/java/org/indtexbr/consultorias/ConsultoriasAssessoriasApplication.java
+++ src/main/java/org/indtexbr/consultorias/ConsultoriasAssessoriasApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConsultoriasAssesoriasApplication {
+public class ConsultoriasAssessoriasApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConsultoriasAssesoriasApplication.class, args);
+		SpringApplication.run(ConsultoriasAssessoriasApplication.class, args);
 	}
 
 }
