d4ee004c8592a1b4e8bc0021919e23d67bd04f51
Diogo Barbosa <diogoalmeidabarbosa@gmail.com>
2021-11-26 04:55:52 -0300

    Adicionando conexão ao banco de dados na AWS

--- src/main/java/org/indtexbr/consultorias/adapters/mappers/ConsultoriaMapper.java
+++ src/main/java/org/indtexbr/consultorias/adapters/mappers/ConsultoriaMapper.java
@@ -8,22 +8,26 @@ public class ConsultoriaMapper {
 	public static ConsultoriaEntity consultoriaDTOToConsultoriaEntity(ConsultoriaDTO consultoriaDTO) {
 		
 		ConsultoriaEntity consultoriaEntity = new ConsultoriaEntity();
-		consultoriaEntity.setIdConsultoria(consultoriaDTO.getIdConsultoria());
-		consultoriaEntity.setNomeConsultoria(consultoriaDTO.getNomeConsultoria());
-		consultoriaEntity.setCnpjConsultoria(consultoriaDTO.getCnpjConsultoria());
-		consultoriaEntity.setDataContratacao(consultoriaDTO.getDataContratacao());
-		consultoriaEntity.setFlagAtiva(consultoriaDTO.getFlagAtiva());
+		if(consultoriaDTO != null) {
+			consultoriaEntity.setIdConsultoria(consultoriaDTO.getIdConsultoria());
+			consultoriaEntity.setNomeConsultoria(consultoriaDTO.getNomeConsultoria());
+			consultoriaEntity.setCnpjConsultoria(consultoriaDTO.getCnpjConsultoria());
+			consultoriaEntity.setDataContratacao(consultoriaDTO.getDataContratacao());
+			consultoriaEntity.setFlagAtiva(consultoriaDTO.getFlagAtiva());
+		}
 		return consultoriaEntity;
 	}
 	
 	public static ConsultoriaDTO consultoriaEntityToConsultoriaDTO(ConsultoriaEntity consultoriaEntity) {
 		
 		ConsultoriaDTO consultoriaDTO = new ConsultoriaDTO();
-		consultoriaDTO.setIdConsultoria(consultoriaEntity.getIdConsultoria());
-		consultoriaDTO.setNomeConsultoria(consultoriaEntity.getNomeConsultoria());
-		consultoriaDTO.setCnpjConsultoria(consultoriaEntity.getCnpjConsultoria());
-		consultoriaDTO.setDataContratacao(consultoriaEntity.getDataContratacao());
-		consultoriaDTO.setFlagAtiva(consultoriaEntity.getFlagAtiva());
+		if(consultoriaEntity != null) {
+			consultoriaDTO.setIdConsultoria(consultoriaEntity.getIdConsultoria());
+			consultoriaDTO.setNomeConsultoria(consultoriaEntity.getNomeConsultoria());
+			consultoriaDTO.setCnpjConsultoria(consultoriaEntity.getCnpjConsultoria());
+			consultoriaDTO.setDataContratacao(consultoriaEntity.getDataContratacao());
+			consultoriaDTO.setFlagAtiva(consultoriaEntity.getFlagAtiva());
+		}
 		return consultoriaDTO;
 	}
 }
